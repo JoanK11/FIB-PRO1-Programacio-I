4519e2438db8265e1c6438762339546204c14416
Joan Caballero <70658820+JoanKnight11@users.noreply.github.com>
2020-11-05 13:13:56 +0100

    Add files via upload

---
+++ P12509_en-Recursive_factorial.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n)
+{
+    if (n < 1)
+        return 1;
+    else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main()
+{
+    int x;
+    while (cin >> x) {
+        cout << factorial(x) << endl;
+    }
+}
