c91125f99a113453ec07b999fe37304357e372e3
Joan Caballero <70658820+JoanKnight11@users.noreply.github.com>
2020-11-05 13:03:30 +0100

    Add files via upload

---
+++ P38045_en-Computing_square_roots_and_squares.cc
@@ -0,0 +1,13 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout.setf(ios::fixed);
+    cout.precision(6);
+    int x;
+    while (cin >> x) {
+        cout << x * x << ' ' << sqrt(x) << endl;
+    }
+}
