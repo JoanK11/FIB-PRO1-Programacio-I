f8c6300f3893890bf886da8ff49eebe85e66a703
Joan Caballero <70658820+JoanKnight11@users.noreply.github.com>
2020-11-05 13:02:19 +0100

    Add files via upload

---
+++ P67454_en-Counting_a's_(2).cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char x;
+    int i = 0;
+    while (cin >> x and x != '.') {
+        if (x == 'a')
+            ++i;
+    }
+    cout << i << endl;
+}
