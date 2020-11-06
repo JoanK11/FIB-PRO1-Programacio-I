57385a9fcf919ea4ab24f9573ac36315c699bc65
Joan Caballero <70658820+JoanKnight11@users.noreply.github.com>
2020-11-06 13:11:36 +0100

    Add files via upload

---
+++ P37760_en-Trigonometry.cc
@@ -0,0 +1,13 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout.setf(ios::fixed);
+    cout.precision(6);
+    double x;
+    while (cin >> x) {
+        cout << sin(x * M_PI / 180) << ' ' << cos(x * M_PI / 180) << endl;
+    }
+}
