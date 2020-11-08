053c98cfb45c90bb8e1211e7ea2b5bb1318faf99
Joan Caballero <70658820+JoanKnight11@users.noreply.github.com>
2020-11-08 11:08:03 +0100

    Add files via upload

---
+++ P17179_en-Statistical_measures.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout.setf(ios::fixed);
+    cout.precision(4);
+
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        int number;
+        cin >> number;
+        double x;
+        cin >> x;
+        double max = x, min = x, avr = x;
+
+        for (int j = 1; j < number; ++j) {
+            cin >> x;
+            if (x > max)
+                max = x;
+            else if (x < min)
+                min = x;
+            avr += x;
+        }
+        cout << min << ' ' << max << ' ' << avr / number << endl;
+    }
+}
