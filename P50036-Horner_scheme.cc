cef9d9e3f09fdca4ce089ce8bea5b8d55374dcf4
Joan Caballero <70658820+JoanKnight11@users.noreply.github.com>
2020-11-28 13:34:03 +0100

    Add files via upload

---
+++ P50036-Horner_scheme.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int evaluate(const vector<int>& p, int x)
+{
+    int last = p.size() - 1;
+    // Comencem el comptador amb el terme independent
+    // Anem arrastrant la potencia de x en valor_x
+    int total = p[0], valor_x = 1;
+    for (int i = 1; i <= last; ++i) {
+        valor_x *= x;
+        total += valor_x * p[i];
+    }
+    return total;
+}
+
+int main()
+{
+    // Agafem el nombre d'elements
+    int n;
+    while (cin >> n) {
+        vector<int> P(n);
+        for (int i = 0; i < n; ++i)
+            cin >> P[i];
+        //Declarem el valor de x
+        int x;
+        cin >> x;
+        cout << evaluate(P, x) << endl;
+    }
+}
