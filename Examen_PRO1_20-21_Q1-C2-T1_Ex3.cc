b3ff6b13ddfe61a90cc01b86b44ddf10dc22c976
Joan Caballero <70658820+JoanKnight11@users.noreply.github.com>
2021-01-07 12:10:54 +0100

    Update Examen_PRO1_20-21_Q1-C2-T1_Ex3.cc

--- Examen_PRO1_20-21_Q1-C2-T1_Ex3.cc
+++ Examen_PRO1_20-21_Q1-C2-T1_Ex3.cc
@@ -1,33 +1,15 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-bool esParell(int a) {
-    return a%2 == 0;
-}
-
-int ultimaPosicio(const vector<int>& v, double x) {
-    int size = v.size();
-    for (int i = size - 1; i >= 0; --i) {
-        if (v[i] < x) return i;
-    }
-    return -1;
+int nombreSenseZeros(int x) {
+    if (x == 0) return 0;
+    else if (x%10 == 0) return nombreSenseZeros(x/10);
+    return x%10 + 10*nombreSenseZeros(x/10);
 }
 
 int main() {
-  int n;
-  while (cin >> n) {
-      vector<int> v(n);
-      int nparells = 0;
-      double mitjana = 0;
-      for (int i = 0; i < n; ++i) {
-          cin >> v[i];
-          if (esParell(v[i]) {
-              ++nparells;
-              mitjana += v[i];
-          }
-      }
-      mitjana /= nparells;
-      cout << ultimaPosicio(v, mitjana) << endl;
+    int x;
+    while (cin >> x) {
+        cout << nombreSenseZeros(x) << endl;
     }
 }
