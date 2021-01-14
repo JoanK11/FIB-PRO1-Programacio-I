0c9ee9c2cbc0a2e0a1d20cd471b1766903f85341
Joan Caballero <70658820+JoanKnight11@users.noreply.github.com>
2021-01-14 11:49:47 +0100

    Add files via upload

---
+++ X99393_ca - Factor alfa.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Asignatura {
+    int id;
+    double creditos;
+    double nota;
+};
+
+struct Alumno {
+    int dni;
+    vector <Asignatura> asigs;
+};
+
+typedef vector <Alumno> Alumnos;
+
+// Pre: --
+// Post: retorna el número d'alumnes a alus que superen el factor alfa
+int supera_alfa(const Alumnos& alus) {
+    int count = 0;
+    int alus_size = alus.size();
+    for (int i = 0; i < alus_size; ++i) {
+        int ass_size = alus[i].asigs.size();
+        double mitjana = 0, tcredits = 0;
+        for (int j = 0; j < ass_size; ++j) {
+            mitjana += alus[i].asigs[j].creditos*alus[i].asigs[j].nota;
+            tcredits += alus[i].asigs[j].creditos;
+        }
+        mitjana /= tcredits;
+        if (mitjana >= 5) ++count;
+    }
+    return count;
+}
+
+// Pre: a l'entrada hi ha informació de m assignatures
+// Post: retorna un vector de Asignatura amb la informació de l'entrada,
+//          mantenint l'ordre d'aparició
+vector <Asignatura> lee_asignaturas(int m) {
+   // el teu codi aquí
+   vector<Asignatura> v(m);
+   for (int i = 0; i < m; ++i) {
+       cin >> v[i].id >> v[i].creditos >> v[i].nota;
+   }
+   return v;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    Alumnos a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i].dni;
+        int m;
+        cin >> m;
+        a[i].asigs = lee_asignaturas(m);
+    }
+    cout << supera_alfa(a) << endl;
+}
