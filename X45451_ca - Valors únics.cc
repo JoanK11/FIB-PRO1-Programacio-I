6c03ac9c01ad848687d904a16dfdf8875f2efefd
Joan Caballero <70658820+JoanKnight11@users.noreply.github.com>
2021-01-15 12:02:05 +0100

    Add files via upload

---
+++ X45451_ca - Valors únics.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> unique(const vector<int>& v) {
+    vector<int> u, w = v;
+    sort(w.begin(), w.end());
+    int size = w.size();
+    if (size > 0) u.push_back(w[0]);
+    for (int i = 1; i < size; ++i) {
+        if (w[i] != w[i - 1]) u.push_back(w[i]); 
+    }
+    return u;
+}
+
+vector<int> read_vector(int n) {
+    vector<int> S(n);
+    for (int i = 0; i < n; ++i) 
+        cin >> S[i];
+    return S;
+}
+
+void write_vector(const vector<int>& v) {
+    int n = v.size(); 
+    cout << "[";
+    for (int i = 0; i < n; ++i) { 
+        if (i > 0) cout << ", ";
+            cout << v[i];
+    }
+    cout << "]";
+}
+
+int main() {
+    int n;
+    while (cin >> n) {
+        vector<int> v = read_vector(n);
+        vector<int> u = unique(v);
+        cout << "unique("; write_vector(v); 
+        cout << ") = "; write_vector(u); 
+        cout << endl;
+    } 
+}    
