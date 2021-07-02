575436dda3bcb53645716c5ffbd9d01c206fcc5c
Joan Caballero <70658820+JoanK11@users.noreply.github.com>
2021-07-02 19:39:38 +0200

    ✓

---
+++ P37469-Descomposicio_horaria_(1).cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int h, m, s;
+    h = n / 3600;
+    m = (n % 3600) / 60;
+    s = n % 60;
+    cout << h << ' ' << m << ' ' << s << endl;
+}
