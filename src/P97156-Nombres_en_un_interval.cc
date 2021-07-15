42e5abd5e81c94ee6ba4c9241de0189a223b2b66
Joan Caballero <70658820+JoanK11@users.noreply.github.com>
2021-07-15 14:45:53 +0200

    ✓

---
+++ src/P97156-Nombres_en_un_interval.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    for (int i = x; i < y; ++i) cout << i << ',';
+    if (x <= y) cout << y;
+    cout << endl;
+}
