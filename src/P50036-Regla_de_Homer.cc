7ac74012789f8d73ca1660beaf6d703a92712a44
Joan Caballero <70658820+JoanK11@users.noreply.github.com>
2021-08-23 08:46:49 +0200

    ✓

--- src/P50036-Regla_de_Homer.cc
+++ src/P50036-Regla_de_Homer.cc
@@ -2,11 +2,10 @@
 #include <vector>
 using namespace std;
 
-int evaluate(const vector<int>& p, int x)
-{
+int evaluate(const vector<int>& p, int x) {
     int last = p.size() - 1;
     // Comencem el comptador amb el terme independent
-    // Anem arrastrant la potencia de x en valor_x
+    // Anem arrastrant la potència de x en valor_x
     int total = p[0], valor_x = 1;
     for (int i = 1; i <= last; ++i) {
         valor_x *= x;
@@ -15,14 +14,13 @@ int evaluate(const vector<int>& p, int x)
     return total;
 }
 /*
-int main()
-{
+// Programa principal per provar el funcionament de la funció
+int main() {
     // Agafem el nombre d'elements
     int n;
     while (cin >> n) {
         vector<int> P(n);
-        for (int i = 0; i < n; ++i)
-            cin >> P[i];
+        for (int i = 0; i < n; ++i) cin >> P[i];
         //Declarem el valor de x
         int x;
         cin >> x;
