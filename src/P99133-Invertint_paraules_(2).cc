ee8f0bf0bd3bd14502fd9369d696a7bc7f4e3a72
Joan Caballero <70658820+JoanK11@users.noreply.github.com>
2021-08-23 08:44:06 +0200

    ✓

--- src/P99133-Invertint_paraules_(2).cc
+++ src/P99133-Invertint_paraules_(2).cc
@@ -2,21 +2,20 @@
 #include <vector>
 using namespace std;
 
-int main()
-{
+int main() {
     // Llegeixo el nombre d'elements que tindrà el vector
     int n;
     cin >> n;
+    
     // Declaro el vector i llegeixo el seu contingut ja invertit
     vector<string> vector(n);
-    for (int i = n - 1; i >= 0; --i)
-        cin >> vector[i];
+    for (int i = n - 1; i >= 0; --i) cin >> vector[i];
+    
     // Amb un for declaro el nombre de repeticions que hauré de fer
     // Amb l'altre giro el contingut del vector dins d'una string
     for (int i = 0; i < n; ++i) {
         string word = vector[i];
-        for (int j = word.length() - 1; j >= 0; --j)
-            cout << word[j];
+        for (int j = word.length() - 1; j >= 0; --j) cout << word[j];
         cout << endl;
     }
 }
